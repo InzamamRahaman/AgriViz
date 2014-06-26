9a219d3e7df1989d571663746025785d2885326a
IzzyRahaman <IzzyRahaman@users.noreply.github.com>
2014-06-26 16:31:20 -0700

    Started working on line charts
    Started working on code to pull data using the REST API and to plot the
    line charts

---
+++ AgriVizPureCss/AgriVizPureCss/commodityApiInfo.js
@@ -0,0 +1,10 @@
+﻿
+
+// Details needed to access the data from the REST API
+var root_path = ""
+var monhtly_commodity_names = root_path + "/crops/monthly/commodity/"
+var monthly_commodity_info = function (commodity_name) { return monhtly_commodity_names + commodity_name }
+var monthly_data_url = function (date) { root_path + "/crops/monthly/" + date.toString() }
+var all_monthly_data = root_path + "/crops/monthly/all"
+
+// 
